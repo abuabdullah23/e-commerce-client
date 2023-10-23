1cba22fe16bb764f6fe5405056ca1ab0fb1a7bf5
abuabdullah23 <abuobaidazayed@gmail.com>
2023-10-23 06:49:49 +0600

    Initial commit with setup tailwind

---
+++ src/components/CustomSlider/CustomSlider.jsx
@@ -0,0 +1,63 @@
+import './CustomSlider.css'
+import { useState, useEffect } from 'react';
+import img1 from '../../assets/images/slider/1.jpg'
+import img2 from '../../assets/images/slider/2.jpg'
+import img3 from '../../assets/images/slider/3.jpg'
+import img4 from '../../assets/images/slider/4.jpg'
+import img5 from '../../assets/images/slider/5.jpg'
+
+const CustomSlider = () => {
+    const items = [img1, img2, img3, img4, img5];
+    const [active, setActive] = useState(0);
+
+    const next = () => {
+        setActive((active + 1) % items.length);
+    };
+
+    const prev = () => {
+        setActive((active - 1 + items.length) % items.length);
+    };
+
+    useEffect(() => {
+        const refreshInterval = setInterval(next, 3000);
+
+        return () => {
+            clearInterval(refreshInterval);
+        };
+    }, [active]);
+
+    const dots = items.map((_, index) => (
+        <li
+            key={index}
+            className={index === active ? 'active' : ''}
+            onClick={() => setActive(index)}
+        ></li>
+    ));
+
+    const sliderStyle = {
+        left: `-${active * 100}%`,
+    };
+
+    return (
+        <div className="slider">
+            <div className="list" style={sliderStyle}>
+                {items.map((src, index) => (
+                    <div key={index} className="item">
+                        <img src={src} alt="" />
+                    </div>
+                ))}
+            </div>
+            <div className="buttons">
+                <button id="prev" onClick={prev}>
+                    &lt;
+                </button>
+                <button id="next" onClick={next}>
+                    &gt;
+                </button>
+            </div>
+            <ul className="dots">{dots}</ul>
+        </div>
+    );
+};
+
+export default CustomSlider;
